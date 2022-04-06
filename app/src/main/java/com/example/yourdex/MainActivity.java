ccd0b494a991929ecb101316e2a7a93fa855c53b
tigranohannessian <your_email_address@example.com>
2022-04-06 15:31:59 +0200

    First commit page 2 (homescreen is still being worked on UI wise)

---
+++ app/src/main/java/com/example/yourdex/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.yourdex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageButton BtnLogin;                      //DECLARATIONS
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {     //UPDATE METHOD
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BtnLogin = findViewById(R.id.BTN_login);         //FIND OBJECT DECLARED AND SET TO UI OBJECT
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Login clicked", Toast.LENGTH_SHORT).show();
+               MoveToHomescreen();
+            }
+        });
+
+
+
+
+
+    }
+
+public void MoveToHomescreen()
+{
+    Intent intent = new  Intent(this, Homescreen.class);
+    startActivity(intent);
+}
+
+}
