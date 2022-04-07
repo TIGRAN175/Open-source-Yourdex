0eb5aa634836ff9c59024d8b0898bf8e3a865c7b
tigranohannessian <your_email_address@example.com>
2022-04-07 17:16:09 +0200

    commit 2
    (added version support for android 12 devices
    New screen for new sets
    2 of the 3 buttons now functional)

--- app/src/main/java/com/example/yourdex/Homescreen.java
+++ app/src/main/java/com/example/yourdex/Homescreen.java
@@ -2,13 +2,39 @@ package com.example.yourdex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Homescreen extends AppCompatActivity {
-
+    private ImageButton AddSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
+
+
+
+        AddSet = findViewById(R.id.Btn_AddSet);         //FIND OBJECT DECLARED AND SET TO UI OBJECT
+
+        AddSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Homescreen.this, "add set clicked", Toast.LENGTH_SHORT).show();
+                MoveToAddSetScreen();
+            }
+        });
+
+
+
+
+    }
+
+    public void MoveToAddSetScreen()
+    {
+        Intent intent = new  Intent(this, NewSetInfoPage.class);
+        startActivity(intent);
     }
 }
