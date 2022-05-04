4963a45e8a8c151757c778cbff4dee869c53476b
Nick <33632340+NikoHero@users.noreply.github.com>
2022-05-04 12:41:22 +0200

    Add files via upload

--- app/src/main/java/com/example/yourdex/MainActivity.java
+++ app/src/main/java/com/example/yourdex/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.yourdex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Credentials;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,16 +19,57 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private ImageButton BtnLogin;                      //DECLARATIONS
     private Button BTN_signup;
+
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+    private TextView eRegister;
+
+    private String Username = "Admin";
+    private String Password = "12345678";
+
+
+
+
+    boolean isValid = false;
+    private int counter = 5;
+
+    SharedPreferences sharedPreferences;
+
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {     //UPDATE METHOD
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        eName = findViewById(R.id.LOGIN_username);
+        ePassword = findViewById(R.id.LOGIN_pass);
+        eLogin = findViewById(R.id.btnLogin);
+
+        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
+
+
+           if(sharedPreferences != null)
+           {
+               String savedUsername = sharedPreferences.getString("Username","");
+               String savedPassword = sharedPreferences.getString("Password","");
+
+           //    SignUp.credentials = new Credentials(savedUsername, savedPassword);
+
+
+
+           }
+
+
+        /*
         BtnLogin = findViewById(R.id.BTN_login);         //FIND OBJECT DECLARED AND SET TO UI OBJECT
         BTN_signup = findViewById(R.id.BTN_signup);
         TextView user = (TextView) findViewById(R.id.LOGIN_username);  // username
         TextView Pass = (TextView) findViewById(R.id.LOGIN_pass);  // password
-
+        */
 
 
 
@@ -33,13 +77,27 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences sp = getApplicationContext().getSharedPreferences("MyUserpreffs", Context.MODE_PRIVATE);
         String usename = sp.getString("username", "");
         String pass = sp.getString("Password", "");
-
+    /*
         user.setText(usename);
         Pass.setText(pass);
+*/
+
 
         BtnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+
+
+                if(inputName.isEmpty() || inputPassword.isEmpty()){
+
+                    //If nothing is entered
+                }
+
+
                 Toast.makeText(MainActivity.this, "Login clicked", Toast.LENGTH_SHORT).show();
                MoveToHomescreen();
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out); // activity transition animation
@@ -63,6 +121,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    private boolean validate(String name, String password){
+        if(name.equals(Username)&& password.equals(Password)){
+    return true;
+        }
+    return  false;
+
+    }
+
+
+
+
+
 
 public void MoveToHomescreen()
 {
--- app/src/main/java/com/example/yourdex/SignUp.java
+++ app/src/main/java/com/example/yourdex/SignUp.java
@@ -6,49 +6,88 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Credentials;
+import android.net.wifi.hotspot2.pps.Credential;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SignUp extends AppCompatActivity {
 
-    SharedPreferences sp;
-    String USERNAME, PASSWORD;
+    //  SharedPreferences sp;
+    //   String USERNAME, PASSWORD;
+    private EditText eRegName;
+    private EditText eRegPassword;
+    private Button eRegister;
+
+    Credentials credentials;
+
+
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor sharedPreferencesEditor;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        TextView Username = (TextView) findViewById(R.id.SignUpUsername);  // username
-        TextView Password = (TextView) findViewById(R.id.signUpPassword);  // password
-        Button SignUpBTN = (Button) findViewById(R.id.signUPBTN);  // sign up button
+        eRegName = findViewById(R.id.SignUpUsername);  // username
+        eRegPassword = findViewById(R.id.signUpPassword);  // password
+        eRegister = findViewById(R.id.signUPBTN);  // sign up button
 
 
-        sp = getSharedPreferences( "MyUserpreffs", (Context.MODE_PRIVATE));
+        sharedPreferences = getApplicationContext().getSharedPreferences("CredentialsDB", MODE_PRIVATE);
+        sharedPreferencesEditor = sharedPreferences.edit();
 
 
-
-        SignUpBTN.setOnClickListener(new View.OnClickListener() {
+        eRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               USERNAME = Username.getText().toString();
-                PASSWORD = Password.getText().toString();
-                SharedPreferences.Editor editor = sp.edit();
 
-                editor.putString("username", USERNAME );
-                editor.putString("Password", PASSWORD );
+            String regUsername = eRegName.getText().toString();
+            String regPassword = eRegPassword.getText().toString();
 
-                editor.commit();
+            if (validate(regUsername, regPassword)) {
 
-                MoveToLogin();
+                credentials = new Credentials(regUsername, regPassword);
+                // Store credendts
+
+                sharedPreferencesEditor.putString("Username", regUsername);
+                sharedPreferencesEditor.putString("Password", regPassword);
+
+                sharedPreferencesEditor.apply();
+
+                startActivity(new Intent(SignUp.this, MainActivity.class));
+                Toast.makeText(SignUp.this, "Registration Successful", Toast.LENGTH_SHORT).show();
             }
+        }
         });
-    }
-    public void MoveToLogin()
-    {
-        Intent intent = new  Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
+    };
+
+
+
+
+    private boolean validate (String username, String password){
+
+        if(username.isEmpty() || password.length()< 10){
+            Toast.makeText(this, "Please enter details again", Toast.LENGTH_SHORT).show();
+       return false;
+
+        }
+
+
+        return false;
+    };
+
+
+
+
+
 }
+
